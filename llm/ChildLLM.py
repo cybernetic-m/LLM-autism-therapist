b26e3dc78c8637094133229c071419d5788330ee
AntonioLissa <lissalattanzio.2154208@studenti.uniroma1.it>
2025-08-29 12:51:10 +0200

    cild_llm simulation

---
+++ llm/ChildLLM.py
@@ -0,0 +1,150 @@
+import sys
+import uuid
+
+sys.path.insert(0, './llm')
+import os
+from datetime import datetime
+import yaml
+import os
+import random
+from datetime import date, timedelta
+import math
+
+# Check the operating system, it is used for the import modules
+if os.name == 'nt':  # 'nt' stands for Windows
+    from llm.llm_api import call_translation_api
+    from llm.TherapistLLM import TherapistLLM
+
+    from neo4j_db.database import KnowledgeGraph
+
+    script_dir = os.path.dirname(__file__)
+    file_path = os.path.join(script_dir, "api_key.txt")
+    with open(file_path, "r") as file:
+        groq_api_key = file.read()
+    with open("../config/llm_config.yaml", "r", encoding="utf-8") as f:
+        prompts = yaml.safe_load(f)
+
+elif os.name == 'posix':  # 'posix' stands for Unix/Linux/MacOS
+    from llm_api import call_translation_api
+    from TherapistLLM import TherapistLLM
+    from database import KnowledgeGraph
+    with open("llm/api_key.txt", "r") as file:
+        groq_api_key = file.read()
+    with open("config/llm_config.yaml", "r", encoding="utf-8") as f:
+        prompts = yaml.safe_load(f)
+
+
+if not groq_api_key:
+    print("API KEY NOT LOADED: please follow the instructions in the README.md file to set up the API key.")
+    sys.exit(1)
+
+system_prompt = prompts["system_prompts"]["child_llm"]
+user_prompt = prompts["user_prompt_templates"]["child_llm"]
+
+class ChildLLM:
+    def __init__(self, model_name):
+        self.system_prompt = system_prompt
+        self.user_prompt = user_prompt
+        self.session_history = ''
+        self.data = None
+        self.model_name = model_name
+        self.last_gesture = ''
+        self.last_response = ''
+        self.last_child_sentence = ''
+
+
+    def respond(self, child_info, session_history):
+        prompt = self.user_prompt.format(child_data = child_info, conversation_history = session_history)
+        print("-- FINAL PROMPT --\n", prompt)
+        llm_response = call_translation_api(api_key=groq_api_key,
+                                            model_name=self.model_name,
+                                            system_prompt_template=self.system_prompt,
+                                            user_prompt_template=prompt,
+                                            temperature=1)
+
+        self.session_history += '\n -Therapist: ' + llm_response
+        self.last_response = llm_response
+        return llm_response
+
+
+
+
+unknown_child = {
+    "child_name": "",
+    "child_surname": "",
+    "child_birth": "",
+    "child_gender": "",
+    "child_nation": "",
+    "child_likes": "",
+    "child_dislikes": "",
+    "previous_activity": "",
+}
+
+names_male = ["Luca", "Marco", "Andrea", "Matteo", "Giovanni"]
+names_female = ["Giulia", "Sara", "Chiara", "Martina", "Elisa"]
+surnames = ["Rossi", "Bianchi", "Verdi", "Neri", "Ferrari"]
+
+def random_birthdate(start_year=2010, end_year=2022):
+    """Genera una data di nascita casuale tra start_year e end_year."""
+    start_date = date(start_year, 1, 1)
+    end_date = date(end_year, 12, 31)
+    delta_days = (end_date - start_date).days
+    birthdate = start_date + timedelta(days=random.randint(0, delta_days))
+    return birthdate.strftime("%Y-%m-%d")
+
+def get_random_child():
+    sex = random.choice(["M", "F"])
+    if sex == "M":
+        name = random.choice(names_male)
+    else:
+        name = random.choice(names_female)
+    surname = random.choice(surnames)
+    birth = random_birthdate()
+    return name, surname, sex, birth
+
+def get_score(start, increment):
+    return str(round(abs(math.sin(start+increment)), 2)) # not negative sin
+
+
+
+if __name__ == '__main__':
+
+    child_llm = ChildLLM(model_name='gemma2-9b-it' )
+    name, surname, sex, birth = get_random_child()
+    kg = KnowledgeGraph()
+    all_data = kg.get_child(name=name, surname=surname)
+    data = None
+    if len(all_data) == 0:
+        data = unknown_child
+        data["child_name"] = name
+        data["child_surname"] = surname
+        data["child_gender"] = sex
+        data["child_birth"] = birth
+    elif len(all_data) > 1:
+        print("Bambino già creato, riprova")
+        exit()
+    else:
+        child = all_data[0]
+        data = {
+            "child_name": child["Name"],
+            "child_surname": child["Surname"],
+            "child_birth": child["Birth"],
+            "child_gender": child["Gender"],
+            "child_nation": child.get("Nation"),  # may be missing and return None
+            "child_likes": child["LIKES"],
+            "child_dislikes": child["DISLIKES"],
+            "previous_activity": child.get("last_activity"),
+        }
+
+    print("-- CHILD DATA --\n", data)
+    therapist = TherapistLLM(model_name='llama-3.3-70b-versatile')
+    therapist.load_data(data)
+
+    conversation_length = 10
+    score_start = random.uniform(0, 3.14) # simulate score
+    increment = random.uniform(0.1, 0.5)
+
+    for i in range(conversation_length):
+        therapist_response = therapist.speak()
+        child_response = child_llm.respond(data, therapist.session_history)
+        therapist.add_child_response(child_response + " [SCORE]: " + get_score(score_start, increment*i))
